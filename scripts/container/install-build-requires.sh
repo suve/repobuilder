143a95ed564c5da43ffc7deb4ba8bdc94cf4ed23
suve <veg@svgames.pl>
2020-04-06 12:38:06 +0200

    Print the number of installed/updated packages when building/updating image

--- scripts/container/install-build-requires.sh
+++ scripts/container/install-build-requires.sh
@@ -19,6 +19,43 @@ while [ "$#" -gt 0 ]; do
 	shift
 done
 
+# -- functions
+
+# Remove the dnf log so we have a clean slate
+rm -f /var/log/dnf.log*
+
+function plural() {
+	local number="$1"
+
+	if [ "${number}" -eq 1 ]; then
+		echo "package"
+	else
+		echo "packages"
+	fi
+}
+
+function dnf_stats() {
+	local all="$(grep -E -e '^(Install|Upgrade)[ ]+[0-9]+[ ]+Packages?$' /var/log/dnf.log)"
+	local installed="$(echo "${all}" | grep '^Install' | grep -E --only-matching '[0-9]+' | sed -e 's/^/+ /g' | xargs expr -- 0)"
+	local upgraded="$(echo "${all}" | grep '^Upgrade' | grep -E --only-matching '[0-9]+' | sed -e 's/^/+ /g' | xargs expr -- 0)"
+
+	if [ "${installed}" -gt 0 ]; then
+		if [ "${upgraded}" -gt 0 ]; then
+			echo "${installed} new $(plural ${installed}) installed, ${upgraded} $(plural ${upgraded}) updated"
+		else
+			echo "${installed} new $(plural ${installed}) installed"
+		fi
+	else
+		if [ "${upgraded}" -gt 0 ]; then
+			echo "${upgraded} $(plural ${upgraded}) updated"
+		else
+			echo "no change"
+		fi
+	fi
+
+	rm /var/log/dnf.log
+}
+
 # -- rpmbuild
 
 if [ "${opt_reuse}" -eq 0 ]; then
@@ -34,7 +71,7 @@ if [ "${opt_reuse}" -eq 0 ]; then
 		message FAIL "image(${dist})" "Installing base packages failed; check \"dnf.log\" for more info"
 		exit 2
 	fi
-	message OK "image(${dist})" "Base packages installed successfully"
+	message OK "image(${dist})" "Base packages installed successfully ($(dnf_stats))"
 fi
 
 # -- update
@@ -48,7 +85,7 @@ if [ "$?" -ne 0 ]; then
 	message FAIL "image(${dist})" "Update failed; check \"dnf.log\" for more info"
 	exit 1
 fi
-message OK "image(${dist})" "Update finished"
+message OK "image(${dist})" "Update finished ($(dnf_stats))"
 
 # -- BuildRequires
 
@@ -65,4 +102,4 @@ if [ "$?" -ne 0 ]; then
 	exit 3
 fi
 
-message OK "image(${dist})" "BuildRequires installed successfully"
+message OK "image(${dist})" "BuildRequires installed successfully ($(dnf_stats))"
